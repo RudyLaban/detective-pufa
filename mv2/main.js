08dccb256d3b71152df121b5141351c8e0ba21f7
RudyLaban <lay971@hotmail.fr>
2022-09-03 00:49:22 +0200

    Fix syntax + délai sur alert compteuse de lettre

--- mv2/main.js
+++ mv2/main.js
@@ -4,16 +4,16 @@ if (document.URL.search("indexes.functions.php") != -1) {
     let firstLetter = prompt('Quel est la première lettre de la fonction recherchée ?');
     let nbrOfLetters = prompt('Quelle est la taille de la fonction recherchée ?');
 
-    // On traite si les champs ne sont pas vide
-    if ((firstLetter != null && firstLetter !== 0) || (nbrOfLetters != null && nbrOfLetters !== 0)) {
-        // Toutes les fonction php dans des element.a
+    // On traite si les champs ne sont pas vides
+    if ((firstLetter != null && firstLetter !== "") || (nbrOfLetters != null && nbrOfLetters !== "")) {
+        // Toutes les fonctions php dans des element.a
         let allFunctions = document.querySelectorAll('li.gen-index ul li a');
-    
+
         for(let i in allFunctions) {
-            
+
             let phpFunc = "";
             let element = allFunctions[i];
-            
+
             if (typeof element.text == "string") {
                 phpFunc = element.text;
                 // Gère les syntax Class::fonction (Ex : ArrayIterator::asort)
@@ -27,15 +27,15 @@ if (document.URL.search("indexes.functions.php") != -1) {
                 }
             }
         }
-    
-        // Suppression des élément restant vide
+
+        // Suppression des éléments restant vide
         let genIndexUL = document.querySelectorAll('li.gen-index ul');
         for(let i in genIndexUL) {
-    
+
             let element = genIndexUL[i];
-    
+
             if(typeof(element) == "object") {
-                
+
                 if(element.children.length == 0) {
                     element.parentNode.remove();
                 }
@@ -45,6 +45,7 @@ if (document.URL.search("indexes.functions.php") != -1) {
         alert('Réactualisez la page quand vous souhaiterez utiliser le module.');
     }
 } else {
-    let lettersCounter = document.querySelectorAll('#grille table tr:first-child td').length; 
-    alert("Le mot à deviner contient "+ lettersCounter +" lettres. \nRendez-vous sur https://www.php.net/manual/fr/indexes.functions.php pour trouver des indices.")
+    setTimeout(() => {
+        alert("Le mot à deviner contient " + document.querySelectorAll('#grille table tr:first-child td').length + " lettres. \nRendez-vous sur https://www.php.net/manual/fr/indexes.functions.php pour trouver des indices.");
+    }, 1500)
 }
